fa6507ff6f48f88330d0b6f381ea00d7b132ef78
robinsonhus0 <jacobprien@googlemail.com>
2019-12-26 13:33:57 +0100

    Initial Commit

---
+++ TorchModeIsOn/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TorchModeIsOn
+//
+//  Created by robinsonhus0 on 26.12.19.
+//  Copyright © 2019 robinsonhus0. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
